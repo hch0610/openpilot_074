c7bab2e528393cc9847370d2f0ac98d1b45da8e0
atom <atom9123@gmail.com>
2020-04-28 18:03:12 +0900

    dd

--- selfdrive/controls/lib/pathplanner.py
+++ selfdrive/controls/lib/pathplanner.py
@@ -364,11 +364,11 @@ class PathPlanner():
             self.angle_steers_des_mpc = angle_steers_des
 
     elif self.model_speed > 250:  # corner
-        self.angle_steers_des_mpc = self.limit_ctrl( org_angle_steers_des, 0.3, angle_steers )
-    elif self.model_speed > 200:  # corner
         self.angle_steers_des_mpc = self.limit_ctrl( org_angle_steers_des, 0.7, angle_steers )
-    elif self.model_speed > 150:  # corner
+    elif self.model_speed > 200:  # corner
         self.angle_steers_des_mpc = self.limit_ctrl( org_angle_steers_des, 1, angle_steers )
+    elif self.model_speed > 150:  # corner
+        self.angle_steers_des_mpc = self.limit_ctrl( org_angle_steers_des, 2, angle_steers )
     elif self.model_speed > 100:  # corner
         self.angle_steers_des_mpc = self.limit_ctrl( org_angle_steers_des, 3, angle_steers )
     else:
